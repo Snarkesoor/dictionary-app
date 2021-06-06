780c100bba44008c191267335b62def1d69d0887
Snarkesoor <78655276+Snarkesoor@users.noreply.github.com>
2021-06-06 13:31:49 +0200

    Added photos

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Phonetics(props) {
 return (
     <div className="Phonetics">
-     {props.phonetics.text} (<a href={props.phonetics.audio} rel="noreferrer" target="_blank">Listen</a>)
+     {props.phonetics.text} <a href={props.phonetics.audio} rel="noreferrer" target="_blank"><button>Listen</button></a>
     </div>
     );
 }
--- src/Search.js
+++ src/Search.js
@@ -2,20 +2,29 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Search.css";
 import Definition from "./Definition";
+import Photos from "./Photos";
 
 export default function Search() {
 let [keyword, setKeyword] = useState("");
 let [definitions, setDefinitions] = useState(null);
+let [photos, setPhotos] = useState(null);
 
     function handleResponse(response) {
-        //console.log(response.data[0].meaning[0].definitions[0].definition);
         setDefinitions(response.data[0]);
+    }
+
+    function handlePexelsResponse(response) {
+        setPhotos(response.data.photos);
     }
 
     function search(event) {
         event.preventDefault();
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`
         axios.get(apiUrl).then(handleResponse);
+
+        let pexelsApiKey ="563492ad6f91700001000001a93e0353dd6947048f999d6aa9fd3851";
+        let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=12`;
+        axios.get(pexelsApiUrl, { headers: {"Authorization" : `Bearer ${pexelsApiKey}` }}).then(handlePexelsResponse);
     }
 
     function handleKeyword(event) {
@@ -31,6 +40,7 @@ let [definitions, setDefinitions] = useState(null);
         placeholder="Type any word" />    
     </form>
     <Definition definitions={definitions}/>
+    <Photos photos={photos}/>
     </div>
     );
 }
